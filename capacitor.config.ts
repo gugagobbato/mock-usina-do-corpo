4a8d8e38c39c952a48a24e788f6fe1571f1bc822
Gustavo Gobbato <gusgobbato10@hotmail.com.br>
2024-04-18 17:25:34 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MOCK - Usina do Corpo',
+  webDir: 'www'
+};
+
+export default config;
